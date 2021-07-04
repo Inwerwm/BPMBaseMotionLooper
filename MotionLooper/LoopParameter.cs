9a2ad6b9ac6206073232e194662419ed1deb422f
Inwerwm <saclainvervm@gmail.com>
2021-07-04 16:45:49 +0900

    BPM設定時にIntervalを計算するようにした

--- MotionLooper/LoopParameter.cs
+++ MotionLooper/LoopParameter.cs
@@ -8,10 +8,28 @@ namespace MotionLooper
 {
     public class LoopParameter
     {
+        private decimal bpm;
+        private decimal interval;
+
         public decimal BaseFrameRate { get; init; }
-        
-        public decimal BPM { get; set; }
-        public decimal Interval { get; set; }
+
+        public decimal BPM
+        {
+            get => bpm;
+            set
+            {
+                bpm = value;
+                Interval = 30 / (value / 60);
+            }
+        }
+        public decimal Interval
+        {
+            get => interval;
+            set
+            {
+                interval = value;
+            }
+        }
 
         public LoopParameter(decimal baseFrameRate)
         {
