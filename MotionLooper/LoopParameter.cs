0cc646daab69be24efa81336e24b4db9c7ec7ce1
Inwerwm <saclainvervm@gmail.com>
2021-07-04 16:20:15 +0900

    空のループパラメータクラスを追加

---
+++ MotionLooper/LoopParameter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotionLooper
+{
+    public record LoopParameter
+    {
+        public int BPM { get; init; }
+        public int BaseFrameRate { get; init; }
+        public double Interval { get; init; }
+    }
+}
