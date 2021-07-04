38dea2f5b6464b1ba091da1a0035f529919b0602
Inwerwm <saclainvervm@gmail.com>
2021-07-04 16:03:53 +0900

    テストプロジェクトを追加

---
+++ MotionLooperTest/LoopTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MotionLooperTest
+{
+    [TestClass]
+    public class LoopTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
