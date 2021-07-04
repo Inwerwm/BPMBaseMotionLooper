d42b3efa686f32579a54dee6153d9c9a9b9adbd2
Inwerwm <saclainvervm@gmail.com>
2021-07-04 20:47:00 +0900

    ループモーション生成テストを追加

---
+++ MotionLooperTest/MotionLoopTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotionLooperTest
+{
+    [TestClass]
+    public class MotionLoopTest
+    {
+        [TestMethod]
+        public void TestMotionLoop()
+        {
+
+        }
+    }
+}
