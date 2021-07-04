04cae1edb2e4ef65dcc0878b590ed735eeb143aa
Inwerwm <saclainvervm@gmail.com>
2021-07-04 17:45:32 +0900

    フレーム複製クラスを作成

---
+++ MotionLooper/FrameDuplicator.cs
@@ -0,0 +1,17 @@
+﻿using MikuMikuMethods.VMD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotionLooper
+{
+    public class FrameDuplicator
+    {
+        public IEnumerable<IVocaloidFrame> Duplicate(IEnumerable<IVocaloidFrame> frames, decimal? interval)
+        {
+            return null;
+        }
+    }
+}
