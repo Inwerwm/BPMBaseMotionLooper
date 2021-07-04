afcbb94411fcdd369cdae786eb9c7cf230984921
Inwerwm <saclainvervm@gmail.com>
2021-07-04 20:25:48 +0900

    設置回数の計算処理を実装

--- MotionLooper/BeatParameter.cs
+++ MotionLooper/BeatParameter.cs
@@ -11,6 +11,6 @@ namespace MotionLooper
         public int Frequency { get; set; }
         public int Beat { get; set; }
         public int BeatCount { get; set; }
-        public int ElementCount { get; set; }
+        public int ElementCount => (int)Math.Ceiling(Beat * BeatCount / (decimal)Frequency);
     }
 }
