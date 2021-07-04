6cf98426cd32cb9a965af83c7e6dbad78a498d8e
Inwerwm <saclainvervm@gmail.com>
2021-07-04 17:42:49 +0900

    モーション複製テストを追加

--- MotionLooperTest/DuplicateMotionTest.cs
+++ MotionLooperTest/DuplicateMotionTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MotionLooper;
+using MikuMikuMethods.VMD;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,19 @@ namespace MotionLooperTest
     [TestClass]
     public class DuplicateMotionTest
     {
+        [TestMethod]
+        public void TestDuplicateFrames()
+        {
+            IEnumerable<IVocaloidFrame> frames = new List<IVocaloidFrame>
+            {
+                new VocaloidMotionFrame("test", 0)
+            };
+            var loopParam = new LoopParameter(30) { Interval = 10m };
+            Duplicator d = new();
+
+            IEnumerable<IVocaloidFrame> duplicatedFrames = d.Duplicate(frames, loopParam.Interval);
+
+            Assert.AreEqual((uint)10, duplicatedFrames.First().Frame);
+        }
     }
 }
