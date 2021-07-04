2c01d5a3a82da591816409b09e1f6cd3c4da884c
Inwerwm <saclainvervm@gmail.com>
2021-07-04 16:28:47 +0900

    間違ってdoubleリテラルにしてたのをdecimalリテラルに修正

--- MotionLooperTest/LoopTest.cs
+++ MotionLooperTest/LoopTest.cs
@@ -10,7 +10,7 @@ namespace MotionLooperTest
         public void TestCalcBPM()
         {
             LoopParameter loop = new() { BPM = 120, BaseFrameRate = 30 };
-            Assert.AreEqual(15d, loop.Interval);
+            Assert.AreEqual(15m, loop.Interval);
         }
     }
 }
