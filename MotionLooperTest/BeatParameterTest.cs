71c1f08cad596691847cc77a30afdda37b6dadc1
Inwerwm <saclainvervm@gmail.com>
2021-07-04 20:20:02 +0900

    設置個数を計算する処理のテストを追加

--- MotionLooperTest/BeatParameterTest.cs
+++ MotionLooperTest/BeatParameterTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MotionLooper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,24 @@ namespace MotionLooperTest
     [TestClass]
     public class BeatParameterTest
     {
+        [TestMethod]
+        public void TestCalcElementCount()
+        {
+            BeatParameter beat = new()
+            {
+                Frequency = 2,
+                Beat = 4,
+                BeatCount = 9
+            };
+            Assert.AreEqual(18, beat.ElementCount);
 
+            beat = new()
+            {
+                Frequency = 6,
+                Beat = 4,
+                BeatCount = 5
+            };
+            Assert.AreEqual(4, beat.ElementCount);
+        }
     }
 }
