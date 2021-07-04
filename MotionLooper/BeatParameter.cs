16c22d09bdfe56d38b8d18a1a83371cdfbc0113b
Inwerwm <saclainvervm@gmail.com>
2021-07-04 20:22:35 +0900

    BeatParameterクラスとプロパティを追加

---
+++ MotionLooper/BeatParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotionLooper
+{
+    public class BeatParameter
+    {
+        public int Frequency { get; set; }
+        public int Beat { get; set; }
+        public int BeatCount { get; set; }
+        public int ElementCount { get; set; }
+    }
+}
