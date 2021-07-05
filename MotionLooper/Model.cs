85801c23a5ed2f40872f5673245ab78c59d61efb
Inwerwm <saclainvervm@gmail.com>
2021-07-05 23:30:35 +0900

    ループモーション生成を実装

--- MotionLooper/Model.cs
+++ MotionLooper/Model.cs
@@ -10,11 +10,30 @@ namespace MotionLooper
 {
     public class Model
     {
+        public BeatParameter BeatParams { get; }
+        public LoopParameter LoopParams { get; }
+        public FrameDuplicator FrameDuplicator { get; }
+
+        public Model()
+        {
+            BeatParams = new();
+            LoopParams = new(30);
+            FrameDuplicator = new();
+        }
+
         private VocaloidMotionData ReadFile(string filePath) =>
             !File.Exists(filePath)                          ? throw new FileNotFoundException() :
             Path.GetExtension(filePath).ToLower() != ".vmd" ? throw new InvalidDataException() :
                                                                     new VocaloidMotionData(filePath);
 
+        private VocaloidMotionData CreateLoopMotion(VocaloidMotionData vmd) =>
+            FrameDuplicator.CreateLoopMotion(vmd, LoopParams, BeatParams);
 
+        public void GenerateLoopMotion(string filePath)
+        {
+            var sourceVMD = ReadFile(filePath);
+            var savePath = Path.Combine(Path.GetDirectoryName(filePath) ?? "", Path.GetFileNameWithoutExtension(filePath) + "_loop.vmd");
+            CreateLoopMotion(sourceVMD).Write(savePath);
+        }
     }
 }
