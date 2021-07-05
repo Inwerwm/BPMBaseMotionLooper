3bf0d9caba09c0b4dcdc18b2cf36eef95caea8eb
Inwerwm <saclainvervm@gmail.com>
2021-07-05 22:55:20 +0900

    FrameDuplicatorテストのクラスを統合

--- MotionLooperTest/DuplicateMotionTest.cs
+++ MotionLooperTest/DuplicateMotionTest.cs
@@ -47,5 +47,27 @@ namespace MotionLooperTest
             Assert.AreEqual(r(3.3m * 4), duplicatedFrames.ElementAt(4).Frame);
             Assert.AreEqual(r(3.3m * 5), duplicatedFrames.ElementAt(5).Frame);
         }
+
+        [TestMethod]
+        public void TestCreateLoopMotion()
+        {
+            VocaloidMotionData vmd = new() { ModelName = "Test" };
+            vmd.MotionFrames.Add(new("test", 0));
+            vmd.MorphFrames.Add(new("mp", 1));
+
+            LoopParameter loop = new(30) { Interval = 10m };
+            BeatParameter beat = new() { Beat = 4, LoopCount = 4, Frequency = 2 };
+            FrameDuplicator duplicator = new();
+
+            VocaloidMotionData loopMotion = duplicator.CreateLoopMotion(vmd, loop, beat);
+
+            Assert.AreEqual(2, vmd.Frames.Count());
+            Assert.AreEqual(beat.ElementCount * 2, loopMotion.Frames.Count());
+
+            Assert.AreEqual((uint)0, loopMotion.MotionFrames.ElementAt(0).Frame);
+            Assert.AreEqual((uint)20, loopMotion.MotionFrames.ElementAt(1).Frame);
+            Assert.AreEqual((uint)1, loopMotion.MorphFrames.ElementAt(0).Frame);
+            Assert.AreEqual((uint)21, loopMotion.MorphFrames.ElementAt(1).Frame);
+        }
     }
 }
