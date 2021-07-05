5b34b7e0b0c52ca793ae1ac14e4ba71d39360c02
Inwerwm <saclainvervm@gmail.com>
2021-07-05 23:05:18 +0900

    ファイル読み込みメソッドを追加

--- MotionLooper/Model.cs
+++ MotionLooper/Model.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MikuMikuMethods.VMD;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace MotionLooper
 {
     public class Model
     {
+        private VocaloidMotionData ReadFile(string filePath) =>
+            !File.Exists(filePath)                          ? throw new FileNotFoundException() :
+            Path.GetExtension(filePath).ToLower() != ".vmd" ? throw new InvalidDataException() :
+                                                                    new VocaloidMotionData(filePath);
+
+
     }
 }
