6af12a264003f12d5b5e16fb239c5c2e31743368
Inwerwm <saclainvervm@gmail.com>
2021-09-15 18:43:05 +0900

    テストデータ生成を実装

--- MotionLooperTest/ReprintInterpolationCurveTest.cs
+++ MotionLooperTest/ReprintInterpolationCurveTest.cs
@@ -1,22 +1,48 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MikuMikuMethods;
 using MikuMikuMethods.VMD;
 using System.Collections.Generic;
 
-[TestClass]
-public class ReprintInterpolationCurveTest
+namespace MotionLooperTest
 {
-    List<IVmdFrame> SourceFrames { get; } = new();
-
-    [ClassInitialize]
-    public void CreateTestData(TestContext context)
+    [TestClass]
+    public class ReprintInterpolationCurveTest
     {
+        List<IVmdFrame> SourceFrames { get; } = new();
 
-    }
+        [TestInitialize]
+        public void CreateTestData()
+        {
+            SourceFrames.Clear();
 
-    [TestMethod]
-    public void TestReprint()
-    {
+            (uint Frame, float X, float Y) early = (0, 0.5f, 0.0f);
+            (uint Frame, float X, float Y) late = (10, 0.0f, 0.5f);
 
+            Add("センター");
+            Add("上半身");
+            Add("下半身");
+
+            void Add(string boneName)
+            {
+                SourceFrames.Add(CreateFrame(boneName, early.Frame, early.X, early.Y));
+                SourceFrames.Add(CreateFrame(boneName, late.Frame, late.X, late.Y));
+            }
+
+            IVmdFrame CreateFrame(string boneName, uint frame, float xPos, float yPos)
+            {
+                var key = new VmdMotionFrame(boneName, frame);
+                foreach (var curve in key.InterpolationCurves)
+                {
+                    curve.Value.EarlyControlePointFloat = (xPos, yPos);
+                    curve.Value.LateControlePointFloat = (1.0f - xPos, 1.0f - yPos);
+                }
+                return key;
+            }
+        }
+
+        [TestMethod]
+        public void TestReprint()
+        {
+
+        }
     }
 }
