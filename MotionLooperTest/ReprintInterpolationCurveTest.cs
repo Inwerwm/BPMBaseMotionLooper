da1d2d3bafdf22a9c6d6766c687783a507190547
Inwerwm <saclainvervm@gmail.com>
2021-09-15 18:22:38 +0900

    補完曲線転写機能のテストクラスを生成

---
+++ MotionLooperTest/ReprintInterpolationCurveTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MikuMikuMethods;
+using MikuMikuMethods.VMD;
+using System.Collections.Generic;
+
+[TestClass]
+public class ReprintInterpolationCurveTest
+{
+    List<IVmdFrame> SourceFrames { get; } = new();
+
+    [ClassInitialize]
+    public void CreateTestData(TestContext context)
+    {
+
+    }
+
+    [TestMethod]
+    public void TestReprint()
+    {
+
+    }
+}
