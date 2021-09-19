61394bdd87c5328176ab33e6c0577f5a32a0991d
Inwerwm <saclainvervm@gmail.com>
2021-09-19 22:21:50 +0900

    2分探索による近傍探索を実装

--- MotionLooper/InterpolationCurveReprinter.cs
+++ MotionLooper/InterpolationCurveReprinter.cs
@@ -2,16 +2,44 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MotionLooper
 {
     public class InterpolationCurveReprinter
     {
-        public IEnumerable<IVmdInterpolatable> Reprint(IEnumerable<IVmdInterpolatable> sourceFrames, IEnumerable<IVmdInterpolatable> targetFrames)
+        public T[] Reprint<T>(IEnumerable<T> sourceFrames, IEnumerable<T> targetFrames) where T : IVmdInterpolatable, IVmdFrame =>
+            targetFrames.Select(target =>
+            {
+                target = (T)target.Clone();
+                var nearestSource = FindNearest(sourceFrames.OrderBy(f => f.Frame), target, (s, t) => (sbyte)(s.Frame > t.Frame ? -1 : s.Frame < t.Frame ? 1 : 0));
+                foreach (var curveType in target.InterpolationCurves.Keys.ToArray())
+                {
+                    target.InterpolationCurves[curveType] = nearestSource.InterpolationCurves[curveType];
+                }
+
+                return target;
+            }).ToArray();
+
+        private T FindNearest<T>(IOrderedEnumerable<T> source, T query, Func<T, T, sbyte> comparer)
         {
-            return null;
+            T[] array = source.ToArray();
+            int mid = array.Length / 2;
+            sbyte r;
+
+            return RecursiveFind(array, query, comparer) ?? array[mid];
+
+            T? RecursiveFind(T[] array, T q, Func<T, T, sbyte> cmp)
+            {
+                if (array.Length < 1)
+                    return default;
+
+                mid = array.Length / 2;
+                r = cmp(array[mid], q);
+
+                return r < 0 ? RecursiveFind(array.Take(mid).ToArray(), q, cmp) :
+                       r > 0 ? RecursiveFind(array.Skip(mid + 1).ToArray(), q, cmp) :
+                       array[mid];
+            }
         }
     }
 }
